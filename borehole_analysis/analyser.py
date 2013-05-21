635a570e4e57b914841a0dc834ca0c06333e5c50
Jess Robertson <jesse.robertson@csiro.au>
2013-05-21 11:14:29 +0800

    Adding AnalyserNode class

--- borehole_analysis/analyser.py
+++ borehole_analysis/analyser.py
@@ -11,56 +11,201 @@
 import numpy
 import sklearn.covariance
 import sklearn.cluster
+import sklearn.manifold
+import matplotlib.pyplot
+import borehole_analysis.plotting
 
-class AnalystError(Exception): pass
+class AnalystError(Exception): 
 
-class Analyst(object):
+    """ Exception class thrown by Analyst class
+    """
+
+    pass
+
+
+class AnalystNode(object):
 
     """ Object which stores one node of an analysis tree
     """
 
-    def __init__(self, borehole=None, mother=None, data_view=None):
-        super(Analyst, self).__init__()
+    def __init__(self, borehole=None, mother=None, data_mask=None):
+        super(AnalystNode, self).__init__()
         if borehole is not None:
             if mother is not None:
-                raise AnalystError('Analyst requires either a borehole OR another analyst instance to get going')
+                raise AnalystError('Analyst requires either a borehole OR '
+                    'another analyst instance to get going')
 
-            # We are at the root of the tree, so generate an analyst which sees the entire borehole dataset
+            # We are at the root of the tree, so generate an analyst which 
+            # sees the entire borehole dataset
             self.borehole = borehole
             self.labels = borehole.labels
-            self.data_view = None
+            self.domain = borehole.domain
+            self.data = borehole.data
+            self.data_mask = None
             self.mother = None
+
         elif mother is not None:
             if borehole is not None:
-                raise AnalystError('Analyst requires either a borehole OR another analyst instance to get going')
-            if data_view is None:
-                raise AnalystError("You need to specify a data view on the mother Analyst instance")
+                raise AnalystError('Analyst requires either a borehole OR '
+                    'another analyst instance to get going')
+            if data_mask is None:
+                raise AnalystError('You need to specify a data view on the '
+                    'mother Analyst instance')
 
             # We are in a node, so copy in views of mother instance
-            self.data = mother.data[data_view]
+            self.domain = mother.domain[data_mask]
+            self.data = mother.data[data_mask]
             self.borehole = mother.borehole
             self.labels = mother.labels
-        else: 
-            raise AnalystError('Analyst requires either a borehole or another analyst instance to get going')
 
-            # Initialise node topology
+        else: 
+            raise AnalystError('Analyst requires either a borehole or another '
+                'analyst instance to get going')
+
+        # Initialise node topology
+        self.daughters = None
+
+        # Analyse the current datasets for clusters
         self.products = {
-            'edge_model': None,
+            'edge_graph': None,
             'correlations': None,
             'clusters': None
         }
-        self.daughters = None
-
+        self.__gen_edge_graph(force=True)
+        self.__gen_correlation_graph(force=True)
+        self.__gen_correlation_clusters(force=True)
+        
     def partition(self):
         """ Partition the analyst instance into daughter nodes
         """
-        # Analyse the current datasets for clusters
-        self.__gen_edge_matrix()
-        self.__gen_correlation_graph()
-        self.__gen_correlation_clusters()
+        pass
 
-    def __gen_correlation_graph(self, borehole_label, force=False):
-        """ Generate connections between signals in a borehole using a graph Lasso algorithm.
+    def get_domain(self):
+        """ Returns a view of the current domain
+        """
+        return self.domain
+
+    def get_signal(self, *keys):
+        """ Returns views of the current data for the given keys. If no keys 
+            are specified, return views for all keys.
+        """
+        if keys is None:
+            keys = self.labels.keys()
+        indices = [self.labels[k][0] for k in keys]
+        return dict((k, self.data.T[i]) for k, i in zip(keys, indices))
+
+    def plot_connection_matrix(self):
+        """ Plots the connection matrix assicated with the data in the current 
+            node.
+
+            :returns: handles to the figure and axes
+        """
+        # Calculate correlation matrix
+        names = self.borehole.get_labels()
+        correlations = self.products['correlations']
+
+        # Plot results
+        side_len = 0.5*len(names)
+        fig = matplotlib.pyplot.figure(figsize=(side_len, side_len))
+        axes = matplotlib.pyplot.gca()
+        image = axes.imshow(correlations, 
+            interpolation='none', 
+            cmap=matplotlib.cm.get_cmap("RdBu_r"))
+        cbar = matplotlib.pyplot.colorbar(image, 
+            fraction=0.2, 
+            shrink=(1 - 0.25))
+        cbar.set_label('Correlation')
+        axes.set_xticks(range(len(names)))
+        axes.set_xticklabels(names, rotation=90,
+            horizontalalignment='center',
+            verticalalignment='top')
+        axes.set_yticks(range(len(names)))
+        axes.set_yticklabels(names)
+        return fig, axes
+
+    def plot_connection_graph(self, embedding=None):
+        """ Plot the clusters and connections between data signals.
+
+            We use manifold learning methods to find a low-dimension embedding 
+            for visualisation. For the methods here we use a dense eigensolver 
+            to achieve reproducibility (since arpack is initialised with 
+            random vectors - the result would be different each time) In 
+            addition, we use a large number of neighbours to capture the large-
+            scale structure.
+
+            This could potentially be sped up significantly by using a sparse 
+            representation, at the cost of introducing some randomness to the 
+            visualisation.
+
+            :param embedding: The model to use to embed the nodes in two-dimensional space. If None, it defaults to 'isomap'.
+            :type embedding: `'lle'` or `'isomap'`
+            :returns: handles to the figure and axes
+        """
+        # Get node infomation
+        names = self.borehole.get_labels()
+        clusters = self.products['clusters']['as_vector'] 
+
+        # Calculate embedding
+        embedding = embedding or 'isomap'
+        if embedding is 'isomap':
+            node_position_model = sklearn.manifold.Isomap(
+                n_components=2, 
+                eigen_solver='dense', 
+                n_neighbors=len(names) - 2)
+        elif embedding is 'lle':
+            node_position_model = sklearn.manifold.LocallyLinearEmbedding(
+                n_components=2, 
+                eigen_solver='dense', 
+                n_neighbors=len(names) - 2)
+        else:
+            raise AnalystError("Embedding argument to plot_connection_graph"
+                "must be one of 'lle' or 'isomap'")
+        embedding = node_position_model.fit_transform(self.data.T).T
+
+        # Plot results
+        fig = matplotlib.pyplot.figure(figsize=(15, 15))
+        borehole_analysis.plotting.plot_connection_graph(
+            names=names,
+            cluster_labels=clusters,
+            embedding=embedding, 
+            correlations=self.products['correlations'])
+        return fig, matplotlib.pyplot.gca()
+
+    def plot_borehole_data(self, keys_to_plot=None):
+        """ Plot the data stored in the current node object
+
+            :returns: handles to the figure and axes
+        """
+        if keys_to_plot is None:
+            keys_to_plot = self.borehole.get_keys()
+
+        # Get data from self
+        domain = self.get_domain()
+        signals = self.get_signal(*keys_to_plot)
+        domain_bounds = (self.domain.min(), self.domain.max())
+
+        # Plot data
+        fig = matplotlib.pyplot.figure(figsize=(20, 1*len(keys_to_plot)))
+        for i, key in enumerate(keys_to_plot):
+            axes = matplotlib.pyplot.subplot(1, len(keys_to_plot), i+1)
+            borehole_analysis.plotting.plot_signal(axes, 
+                signal=signals[key], 
+                domain=domain,
+                orientation='vertical')
+            axes.set_xlabel("")
+            if i == 0:
+                axes.set_ylabel('Depth (m)')
+            else:
+                axes.set_ylabel("")
+                axes.set_yticklabels("")
+            axes.set_ylim(domain_bounds)
+            axes.set_title(self.labels[key][1])
+        fig.tight_layout()
+        return fig, axes
+
+    def __gen_correlation_graph(self, force=False):
+        """ Generate connections between signals in a borehole using a graph 
+            Lasso algorithm.
 
             This returns the correlation graph C, which is a strictly lower-
             triangular matrix with the correlation between the ith and jth 
@@ -70,13 +215,13 @@ class Analyst(object):
             :type borehole_label: str
             :returns: the correlation graph C and the fitted edge model
         """
-        if self.products['edge_model'] is not None and not force:
+        if self.products['correlations'] is not None and not force:
             return
-        self.print_info('Generating covariance graph', 'info')
-        self.__gen_edge_matrix()
+        self.borehole.print_info('Generating covariance graph', 'info')
+        self.__gen_edge_graph()
 
         # Get the correlations from the model
-        correlations = self.products['edge_model'].precision_.copy()
+        correlations = self.products['edge_graph'].precision_.copy()
         determinant = 1 / numpy.sqrt(numpy.diag(correlations))
         correlations *= determinant
         correlations *= determinant[:, numpy.newaxis]
@@ -85,85 +230,76 @@ class Analyst(object):
         # perfectly correlated with itself). Also remove half the graph since
         # the results are symmetric
         correlations = -numpy.tril(correlations, k=-1)
-        correlations = numpy.ma.masked_array(correlations,
+        self.products['correlations'] = numpy.ma.masked_array(correlations,
             mask=(numpy.abs(correlations) <= 0.02))
-        self[borehole_label]['correlations'] = correlations
 
-    def __gen_edge_matrix(self, force=False):
+    def __gen_edge_graph(self, force=False):
         """ Generate an edge matrix structure for the current borehole 
         """
-        if self.products['edge_model'] is not None and not force:
+        if self.products['edge_graph'] is not None and not force:
             return
-        self.print_info('Generating edge connections', 'info')
-        self.products['edge_model'] = \
+        self.borehole.print_info('Generating edge connections', 'info')
+        self.products['edge_graph'] = \
             sklearn.covariance.GraphLassoCV()
-        self.products['edge_model'].fit(borehole.data)
+        self.products['edge_graph'].fit(self.data)
 
     def __gen_correlation_clusters(self, force=False):
         """ Generate signal clusters using signal correlations as a metric.
 
             :param borehole: The borehole data
             :type borehole: `borehole_analysis.Borehole`
-            :returns: a set of clusters as a dictionary, where the key is the cluster label, and the value is a list of data labels from the Borehole instance 
+            :returns: a set of clusters as a dictionary, where the key is the 
+                cluster label, and the value is a list of data labels from the 
+                Borehole instance 
         """
-        if self[borehole_label]['clusters'] is not None and not force:
+        if self.products['clusters'] is not None and not force:
             return
 
         # Generate clusters
-        self.print_info('Generating covariance clusters', 'info')
-        self.__gen_edge_model()
-        _, labels = sklearn.cluster.affinity_propagation(
-                                    self.['edge_model'].covariance_)
+        self.borehole.print_info('Generating covariance clusters', 'info')
+        self.__gen_edge_graph()
+        _, cluster_labels = sklearn.cluster.affinity_propagation(
+                                    self.products['edge_graph'].covariance_)
 
         # Store results as dictionary - we have two values, one for human 
         # consumption and one by key for looping
-        names = [v[1] for v in self.labels.values()]
+        keys = self.borehole.get_keys()
+        labels = self.borehole.get_labels()
         self.products['clusters'] = {
-            'by_label': dict([(li, [n for l, n in zip(labels, names) 
+            'by_label': dict([(li, [n for l, n in zip(cluster_labels, labels) 
                                       if l == li]) 
-                              for li in range(labels.max() + 1)]),
-            'by_key': dict([(li, [k for l, k in zip(labels, self.labels.keys())
+                              for li in range(cluster_labels.max() + 1)]),
+            'by_key': dict([(li, [k for l, k in zip(cluster_labels, keys)
                                     if l == li]) 
-                             for li in range(labels.max() + 1)]),
+                             for li in range(cluster_labels.max() + 1)]),
+            'as_vector': cluster_labels
         }
         
         # Print information if required
-        self.print_info('Clusters: \n' 
+        self.borehole.print_info('Clusters: \n' 
             + '\n'.join(['   -- {0}: {1}'.format(l, c)
                 for l, c in self.products['clusters']['by_label'].items()]),
             'info')
 
-    def print_info(self, message, flag=None):
-        """ Print a message if we're tracking transformations
-        """
-        header_dict = {
-            'default': '         ',
-            'info': '   info: ',
-            'warn': 'warning: '
-        }
-        if self.borehole.verbose:
-            try:
-                header = header_dict[flag]
-            except KeyError:
-                header = header_dict['default']
-            print header, message
 
-
-class Analyser(dict):
+class Analyst(dict):
     
     """ Object to run analysis over sets of boreholes
 
-        :param **boreholes: A list of boreholes given as keyword arguments, where the keyword is the borehole label, and the value is a borehole_analysis.Borehole instance.
+        :param boreholes: A dict of boreholes, where the keyword is the 
+            borehole label, and the value is a borehole_analysis.Borehole 
+            instance.
+        :type boreholes: dict
     """
     
-    def __init__(self, **boreholes):
-        super(Analyser, self).__init__()
+    def __init__(self, boreholes):
+        super(Analyst, self).__init__()
 
         # Store boreholes, and generate a root Analyst instance to get 
         # things going
         for label, borehole in boreholes.items():
             self[label] = {
-                'borehole': borehole
-                'analyst_tree': Analyst(borehole)
+                'borehole': borehole,
+                'analyst_tree': AnalystNode(borehole)
             }
             self[label]['analyst_tree'].partition()
