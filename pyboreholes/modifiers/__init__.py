c32ab61f746c161af68c49d374ca079777326168
Jess Robertson <jesse.robertson@csiro.au>
2013-11-10 17:53:15 +1100

    Adding layer metrics in own file

---
+++ pyboreholes/modifiers/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+""" file:   __init__.py (pyboreholes.modifiers)
+    author: Jess Robertson
+            CSIRO Earth Science and Resource Engineering
+    email:  jesse.robertson@csiro.au
+    date:   Wednesday May 1, 2013
+
+    description: Initialisation of the pyboreholes.modifiers module.
+"""
+
+from .label_tree import LabelTree, metrics
+from .regularizer import *
