81663a6002f33721b635fa05960ab168c93fa915
Jess Robertson <jesse.robertson@csiro.au>
2013-11-10 13:12:03 +0800

    New structure for domain classes

---
+++ pyboreholes/domains/interval_domain.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+""" file: interval_domain.py (pyboreholes.domains)
+    author: Jess Robertson
+            CSIRO Earth Science and Resource Engineering
+    date: Sunday November 10, 2013
+
+    description: Domain for interval data (data defined over an interval in 
+        the borehole).
+"""
+
+from .domain import Domain
---
+++ pyboreholes/domains/sampling_domain.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+""" file: sampling_domain.py (pyboreholes.domains)
+    author: Jess Robertson
+            CSIRO Earth Science and Resource Engineering
+    date: Sunday November 10, 2013
+
+    description: Domain for sampling data (data sampled at a finite set of 
+            points down a borehole)
+"""
+
+import pywt
