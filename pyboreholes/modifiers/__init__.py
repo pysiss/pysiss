2ebdef931b7cfea3935fa397d43f528d7a52d408
Jess Robertson <jesse.robertson@csiro.au>
2013-11-10 17:54:40 +1100

    Update LabelTree class to use metrics file

--- pyboreholes/modifiers/__init__.py
+++ pyboreholes/modifiers/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-""" file:   __init__.py (pyboreholes.modifiers)
+""" file:   __init__.py (pyboreholes.analysis`)
     author: Jess Robertson
             CSIRO Earth Science and Resource Engineering
     email:  jesse.robertson@csiro.au
@@ -8,5 +8,5 @@
     description: Initialisation of the pyboreholes.modifiers module.
 """
 
-from .label_tree import LabelTree, metrics
+from .layers import LabelTree, metrics
 from .regularizer import *
