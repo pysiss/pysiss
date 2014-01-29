191a48d48dbecbf37c01f5fe2cb281c590b987bd
Jess Robertson <jesse.robertson@csiro.au>
2014-01-29 11:17:39 +0800

    Add add_domain method
    Added an add_domain method to Borehole class which automatically assigns
    a supplied domain to the correct domain listing. This means that domains
    can now be constructed by an importer and easily added, which is useful
    when your data is coming in from different files for the same borehole..

--- pyboreholes/borehole.py
+++ pyboreholes/borehole.py
@@ -10,6 +10,7 @@
 from .domains import SamplingDomain, IntervalDomain, WaveletDomain
 from .properties import Property
 
+
 class Borehole(object):
 
     """ Class to represent a borehole.
@@ -51,31 +52,43 @@ class Borehole(object):
         :type name: `string`
     """
 
+    # Mapping domain types to class attributes
+    _type_to_attr = {
+        Domain: 'domains',
+        SamplingDomain: 'sampling_domains',
+        IntervalDomain: 'interval_domains',
+        WaveletDomain: 'wavelet_domains',
+    }
+
     def __init__(self, name):
         self.name = name
         self.collar_location = None
         self.survey = None
         self.features = dict()
-        self.interval_domains = dict()
-        self.sampling_domains = dict()
-        self.wavelet_domains = dict()
+
+        # Initialize domain lists
+        for domain_attr in self._type_to_attr.values():
+            setattr(self, domain_attr, dict())
 
     def __repr__(self):
         """ String representation
         """
-        info_str = (
-            'Borehole {0}: {1}/{2} interval/sampling domains & {3} '
-            + 'features'
-            + '\nIDs: '
-            + '\n     '.join(map(str, self.interval_domains.values()))
-            + '\nSDs: '
-            + '\n     '.join(map(str, self.sampling_domains.values()))
-            + '\nWDs: '
-            + '\n     '.join(map(str, self.wavelet_domains.values()))
-        )
-        return info_str.format(self.name,
-            len(self.interval_domains), len(self.sampling_domains),
-            len(self.features))
+        info_str = 'Borehole {0} contains '.format(self.name)
+        n_domains = sum([len(getattr(self, a))
+                         for a in self._type_to_attr.values()])
+        summary_str = '{0} domains'.format(n_domains)
+        summary_str += ' & {0} features'.format(len(self.features))
+        domain_list = ''
+        if len(self.interval_domains) > 0:
+            domain_list += ('\nIDs: ' + '\n     '.join(
+                            map(str, self.interval_domains.values())))
+        if len(self.sampling_domains) > 0:
+            domain_list += ('\nSDs: ' + '\n     '.join(
+                            map(str, self.sampling_domains.values())))
+        if len(self.wavelet_domains) > 0:
+            domain_list += ('\nWDs: ' + '\n     '.join(
+                            map(str, self.wavelet_domains.values())))
+        return info_str + summary_str + domain_list
 
     def add_feature(self, name, depth):
         """ Add and return a new Feature.
@@ -89,7 +102,19 @@ class Borehole(object):
         self.features[name] = Feature(name, depth)
         return self.features[name]
 
-    def add_interval_domain(self, name, from_depth, to_depths):
+    def add_domain(self, domain):
+        """ Add an existing domain instance to the borehole.
+
+            :param domain: A precooked domain with data
+            :type domain: subclassed from `pyboreholes.Domain`
+        """
+        # Work out which attribute we should add the domain to
+        add_to_attr = self._type_to_attr[type(domain)]
+
+        # Add to the given attribute using the domain name as a key
+        getattr(self, add_to_attr)[domain.name] = domain
+
+    def add_interval_domain(self, name, from_depths, to_depths):
         """ Add and return a new IntervalDomain
 
             :param name: The identifier for the new IntervalDomain
@@ -97,13 +122,14 @@ class Borehole(object):
             :param from_depths: Interval start point down-hole depths in metres
                     from collar
             :type from_depths: iterable of numeric values
-            :param to_depths: Interval end point down-hole depths in metres from collar
+            :param to_depths: Interval end point down-hole depths in metres
+                from collar
             :type to_depths: iterable of numeric values
 
             :returns: the new `pyboreholes.IntervalDomain` instance.
         """
         self.interval_domains[name] = \
-            IntervalDomain(name, from_depth, to_depths)
+            IntervalDomain(name, from_depths, to_depths)
         return self.interval_domains[name]
 
     def add_sampling_domain(self, name, depths):
@@ -120,11 +146,11 @@ class Borehole(object):
         return self.sampling_domains[name]
 
     def add_wavelet_domain(self, name, sampling_domain,
-        wavelet=None, wav_properties=None):
+                           wavelet=None, wav_properties=None):
         """ Add and return a new WaveletDomain.
         """
         self.wavelet_domains[name] = WaveletDomain(name, sampling_domain,
-            wavelet, wav_properties)
+                                                   wavelet, wav_properties)
         return self.wavelet_domains[name]
 
     def desurvey(self, depths, crs):
@@ -178,11 +204,12 @@ class Feature(object):
         """
         return self.properties.keys()
 
+
 class CoordinateReferenceSystem(object):
 
     """System for describing a spatial location as a tuple of real numbers."""
 
-    def  __init__(self):
+    def __init__(self):
         raise NotImplementedError
 
 
