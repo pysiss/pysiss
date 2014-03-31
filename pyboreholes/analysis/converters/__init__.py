431d2a3d1a70952587c8454aaca53933c218a059
Jess Robertson <jesse.robertson@csiro.au>
2014-03-31 14:03:13 +0800

    DETCRC-27: Fix overwritten TimeDomain instance

--- pyboreholes/analysis/converters/__init__.py
+++ pyboreholes/analysis/converters/__init__.py
@@ -1,10 +1,10 @@
-""" file:   __init__.py (pyboreholes.analysis.time_conversion)
+""" file:   __init__.py (pyboreholes.analysis.converters)
     author: Jess Robertson
             CSIRO Earth Science and Resource Engineering
     email:  jesse.robertson@csiro.au
     date:   Friday March 28, 2014
 
-    description: Initialisation of the pyboreholes.analysis.time_conversion
+    description: Initialisation of the pyboreholes.analysis.converters
         module.
 """
 
