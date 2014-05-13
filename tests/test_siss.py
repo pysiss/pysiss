679fe5ec5fe6be58705ed6bad4a94f3aec19585f
Jess Robertson <jesse.robertson@csiro.au>
2014-05-13 16:06:26 +0800

    DARDA-106: Fixing a bug in the SISS unittests
    The output for SISSBoreholeGenerator is different to expected in the test. As per Dave's previous commit, this should be changed.

--- tests/test_siss.py
+++ tests/test_siss.py
@@ -1,7 +1,7 @@
 """ file: test_siss.py
 
     author: David Benn
-            CSIRO IM&T Science Data Services 
+            CSIRO IM&T Science Data Services
     date: 18 February 2014
 
     description: Test of siss module for creating PropertyType objects from
@@ -18,34 +18,39 @@ import unittest
 
 from pprint import pprint
 
+
 class SissTest(unittest.TestCase):
 
     def setUp(self):
         self.siss = pybh.SISSBoreholeGenerator()
 
     def test_geosciml_nvcl_scanned_borehole(self):
+        """ A test using one of the scanned borehole GeoSciML URLs returned by
+            nvcl.get_borehole_ids()
         """
-        A test using one of the scanned borehole GeoSciML URLs returned by 
-        nvcl.get_borehole_ids()
-        """        
-        bh_urls = ['http://nvclwebservices.vm.csiro.au/resource/feature/CSIRO/borehole/WTB5']
+        bh_urls = ['http://nvclwebservices.vm.csiro.au/resource/feature/' +
+                   'CSIRO/borehole/WTB5']
         boreholes = []
-        
-        for bh_url in bh_urls:
-            bh_name = bh_url[bh_url.rfind('/')+1:]
-            boreholes.append(self.siss.geosciml_to_borehole(bh_name, urllib.urlopen(bh_url)))
-            
-        expected_positions = {u'WTB5': u'Origin position: latitude -28.4139, longitude 121.142'}
 
-        expected_details = {u'WTB5': u"Borehole detail: BoreholeDetail(name='drilling method', values='diamond core', property_type=None)"}
-        
+        for bh_url in bh_urls:
+            bh_name = bh_url[bh_url.rfind('/') + 1:]
+            boreholes.append(
+                self.siss.geosciml_to_borehole(bh_name,
+                                               urllib.urlopen(bh_url)))
+
+        expected_positions = {u'WTB5': u'latitude -28.4139, longitude 121.142'}
+
+        expected_details = {u'WTB5': u"BoreholeDetail(name='drilling method', "
+                            + "values='diamond core', property_type=None)"}
+
         for borehole in boreholes:
-            self.assertEquals(expected_positions[borehole.name], str(borehole.origin_position))
-            self.assertEquals(expected_details[borehole.name], str(borehole.details))
-            
+            self.assertEquals(expected_positions[borehole.name],
+                              str(borehole.origin_position))
+            self.assertEquals(expected_details[borehole.name],
+                              str(borehole.details))
+
     def _test_wfs(self):
-        """
-        WFS test (exploratory)
+        """ WFS test (exploratory)
         """
         providerkey = 'CSIRO'
         endpoint = nvcl.NVCL_ENDPOINTS[providerkey]
@@ -57,6 +62,6 @@ class SissTest(unittest.TestCase):
         print matches
         matches = xmltree.findall('.//{http://www.opengis.net/gml}Point')
         print matches
-        matches = xmltree.findall('.//{urn:cgi:xmlns:CGI:GeoSciML:2.0}Borehole')
+        matches = xmltree.findall(
+            './/{urn:cgi:xmlns:CGI:GeoSciML:2.0}Borehole')
         print matches
-        
