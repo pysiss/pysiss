3107e702664d5f2a3ecb036a2f14d6bdfc5f0a2f
Jess Robertson <jesse.robertson@csiro.au>
2014-05-14 15:38:02 +0800

    Fixing test running

--- build_all.sh
+++ build_all.sh
@@ -27,34 +27,35 @@ find . -name "*.pyc" -delete >> ${build_log}
 rm -rf dist >> ${build_log}
 rm -rf build >> ${build_log}
 rm -rf *.egg-info >> ${build_log}
-python setup.py clean >> ${build_log}
+python setup.py clean >> ${build_log} 2>&1
 echo "" >> ${build_log}
 
 # Build
 echo " --> Building..." | tee -a ${build_log}
-python setup.py install --user >> ${build_log}
+python setup.py install --user >> ${build_log} 2>&1
 echo "" >> ${build_log}
 
 # Run unittests
 echo " --> Running unit tests..." | tee -a ${build_log} ${test_log}
 echo "     Output in ${test_log}" | tee -a ${build_log}
-coverage run --source=pyboreholes -m unittest tests >> ${test_log} 2>&1
+coverage run --source=pyboreholes tests >> ${test_log} 2>&1
 if [[ `cat ${test_log} | grep -c "FAILED"` -ne 0 ]]; then
-    echo "     WARNING: some tests failed! " \
+    echo " --> WARNING: some tests failed! " \
         | tee -a ${build_log} ${test_log}
 else
-    echo "     All tests passed." | tee -a ${build_log} ${test_log}
+    echo " --> All tests passed." | tee -a ${build_log} ${test_log}
 fi
-echo "" | tee -a ${test_log} ${build_log}
 
 # Check test coverage if coverage.py is in path
 path_to_coverage=$(which coverage)
 if [[ -x "$path_to_coverage" ]] ; then
+	echo "" >> ${test_log}
 	echo "Test coverage:" >> ${test_log}
 	echo "" >> ${test_log}
 	coverage report >> ${test_log}
 	echo "" >> ${test_log}
 else
+	echo "" | tee -a ${test_log}
 	echo " --> Couldn't find coverage.py on the current path" | tee -a ${test_log}
 	echo "     Skipping code coverage tests" | tee -a ${test_log}
 	echo "     To check test coverage, install coverage.py " | tee -a ${test_log}
