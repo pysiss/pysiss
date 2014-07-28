1ebfbfad9cb98ba74f2ad2b63540688b68716fce
Jess Robertson <jesse.robertson@csiro.au>
2014-07-28 09:51:54 +0800

    Adding init file so pysiss builds properly

---
+++ pysiss/__init__.py
@@ -0,0 +1,13 @@
+""" file:   __init__.py (pysiss)
+    author: Jess Robertson
+            CSIRO Minerals Resources Flagship
+    date:   Monday 28 July 2014
+
+    description: Init file for pysiss
+"""
+
+import borehole
+import coverage
+import vocabulary
+
+__all__ = [borehole, coverage, vocabulary]
