208347ca51cda4edd1c8d8f28529db069f328444
Jess Robertson <jesse.robertson@csiro.au>
2014-08-12 11:38:14 +0800

    PYSISS-1: Fixing imports of lithology module
    Imports weren't happening automatically for lithology. Fixed.

---
+++ pysiss/vocabulary/lithology/__init__.py
@@ -0,0 +1,11 @@
+""" file:   __init__.py (pysiss.vocabulary.composition)
+    author: Jess Robertson
+            Mineral Resources Flagship, CSIRO
+    date:   something
+
+    description: init file
+"""
+
+import composition
+
+__all__ = [composition]
