dd55d615c612e371ffeb12212f2883b6bd2edc6f
Jess Robertson <jesse.robertson@csiro.au>
2014-08-12 11:26:14 +0800

    Fixing imports

--- pysiss/borehole/__init__.py
+++ pysiss/borehole/__init__.py
@@ -9,7 +9,13 @@
 
 from .borehole import Borehole, Feature
 from .borehole_collection import BoreholeCollection
-from .datasets import DataSet, PointDataSet, IntervalDataSet 
+from .datasets import DataSet, PointDataSet, IntervalDataSet
 from .properties import Property, PropertyType
 from pysiss.borehole.siss.borehole_generator import SISSBoreholeGenerator
-from . import importers, plotting, analysis, utilities
+from . import importers, plotting, analysis
+
+__all__ = [Borehole, Feature, BoreholeCollection,
+           DataSet, PointDataSet, IntervalDataSet,
+           Property, PropertyType,
+           SISSBoreholeGenerator,
+           importers, plotting, analysis]
