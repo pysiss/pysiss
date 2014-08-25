ef8c9f9b421d9fb2371777f449f460b15ba5affa
Jess Robertson <jesse.robertson@csiro.au>
2014-08-25 17:13:11 +0800

    PYSISS-9: Putting the GSML unmarshallers back
    Deleted in a previous commit for some reason

--- pysiss/vocabulary/gsml/unmarshallers.py
+++ pysiss/vocabulary/gsml/unmarshallers.py
@@ -7,10 +7,61 @@
 """
 
 from ..utilities import xml_namespaces
+from ..gml.unmarshallers import UNMARSHALLERS as GML_UNMARSHALLERS
 
 NAMESPACES = xml_namespaces.NamespaceRegistry()
 
 
-UNMARSHALLERS = {}
+def shape(elem):
+    """ Unmarshal a gsml:shape element
+
+        Here we just pass through to underlying gml shape data
+    """
+    child = elem[0]
+    unmarshal = GML_UNMARSHALLERS[xml_namespaces.shorten_namespace(child.tag)]
+    return unmarshal(child)
+
+
+def get_value(elem):
+    """ Unmashall an element containing a gsml:value element somewhere in its
+        descendents.
+
+        Returns the text value for a given element, stripping out children of
+        the given element
+    """
+    result = elem.xpath('.//gsml:value/text()',
+                        namespaces=NAMESPACES)
+    if result:
+        return result[0]
+    else:
+        return None
+
+
+def cgi_termrange(elem):
+    """ Unmarshal a gsml:CGI_TermRange element
+
+        Return the value range for a given element
+    """
+    return map(get_value,
+               elem.xpath('.//gsml:CGI_TermValue',
+                          namespaces=NAMESPACES))
+
+
+def sampling_frame(elem):
+    """ Unmarshal a gsml:samplingFrame element
+    """
+    return elem.get(xml_namespaces.expand_namespace('xlink:href'))
+
+
+UNMARSHALLERS = {
+    'gsml:shape': shape,
+    'gsml:value': get_value,
+    'gsml:CGI_TermValue': get_value,
+    'gsml:CGI_TermRange': cgi_termrange,
+    'gsml:preferredAge': get_value,
+    'gsml:observationMethod': get_value,
+    'gsml:positionalAccuracy': get_value,
+    'gsml:samplingFrame': sampling_frame
+}
 
 __all__ = (UNMARSHALLERS,)
