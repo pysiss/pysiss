c4253d5e114c65a2b09e1be18fc8198eb64c5dfe
Jess Robertson <jesse.robertson@csiro.au>
2014-08-25 09:18:07 +0800

    PYSISS-9: Add utilities for dealing with XML namespaces

--- pysiss/vocabulary/utilities/xml_namespaces.py
+++ pysiss/vocabulary/utilities/xml_namespaces.py
@@ -1,4 +1,4 @@
-""" file:   xml_namespaces.py
+""" file: xml_namespaces.py
     author: Jess Robertson
             CSIRO Mineral Resources Flagship
     date:   Monday 25 August, 2014
@@ -94,45 +94,3 @@ def split_namespace(tag):
 
 __all__ = [add_namespace, shorten_namespace,
            expand_namespace, split_namespace]
-
-
-if __name__ == '__main__':
-    import unittest
-
-    class TestXMLNamespaces(unittest.TestCase):
-
-        """ Tests for XML namespace munging
-        """
-
-        def test_split_namespace(self):
-            """ Test namespace splitting returns the right values
-            """
-            expected = ('urn:cgi:xmlns:CGI:GeoSciML:2.0', 'MappedFeature')
-            self.assertEqual(
-                split_namespace('urn:cgi:xmlns:CGI:GeoSciML:2.0:MappedFeature'),
-                expected)
-            self.assertEqual(
-                split_namespace('{urn:cgi:xmlns:CGI:GeoSciML:2.0}MappedFeature'),
-                expected)
-            self.assertEqual(
-                split_namespace('gsml:MappedFeature'),
-                ('gsml', 'MappedFeature'))
-
-        def test_shorten_namespace(self):
-            """ Check namespace shortening
-            """
-            self.assertEqual(
-                shorten_namespace('{http://www.opengis.net/gml}boundedBy'),
-                'gml:boundedBy')
-
-        def test_expand_namespace(self):
-            """ Check namespace expansion
-            """
-            self.assertEqual(
-                expand_namespace('gsml:MappedFeature'),
-                '{urn:cgi:xmlns:CGI:GeoSciML:2.0}MappedFeature')
-            self.assertEqual(
-                expand_namespace('gsml:MappedFeature', form='rdf'),
-                'urn:cgi:xmlns:CGI:GeoSciML:2.0:MappedFeature')
-
-    unittest.main()
