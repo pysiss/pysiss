20ffeea8e65cad7fc6db7e4d0d9aaac2c2bc0ce1
Jess Robertson <jesse.robertson@csiro.au>
2014-08-27 12:33:00 +0800

    Adding importers to base level module

---
+++ pysiss/importers/__init__.py
@@ -0,0 +1,12 @@
+""" file:   __init__.py (pysiss.borehole.importers)
+    author: Jess Robertson
+            CSIRO Earth Science and Resource Engineering
+    email:  jesse.robertson@csiro.au
+    date:   Wednesday May 1, 2013
+
+    description: Initialisation of the pysiss.borehole.importers module.
+"""
+
+import nvcl
+
+__all__ = [nvcl]
