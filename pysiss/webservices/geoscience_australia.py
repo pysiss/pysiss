1b75e8cd935048503858e1531c047f225583a231
Jess Robertson <jesse.robertson@csiro.au>
2014-09-02 15:51:37 +0800

    PYSISS-14: Adding GA endpoints

--- pysiss/webservices/geoscience_australia.py
+++ pysiss/webservices/geoscience_australia.py
@@ -3,17 +3,17 @@
             CSIRO Mineral Resources National Research Flagship
     date:   2 September 2014
 
-    description: Importer for NVCL data services
+    description: Importer for Geoscience Australia data services
 """
 
 # Add 2.5M geology
-wfsurl_template = ()
-
-# Add WA faults
-wfsurl_template = "http://www.ga.gov.au/geows/{0}/oneg_wa_1m/wfs"
-geologic_objects = ('contacts', 'faults', 'geologicunits')
-
-
 DEFAULT_ENDPOINTS = {
-    'wa_faults_1m': 
+    'wa1m': dict(
+        url_template="http://www.ga.gov.au/geows/{0}/oneg_wa_1m/wfs",
+        geologic_objects=('contacts', 'faults', 'geologicunits')
+    ),
+    'aus2.5m': dict(
+        url_template="http://www.ga.gov.au/geows/{0}/oneg_aus_2_5m/wfs",
+        geologic_objects=('contacts', 'faults', 'geologicunits', 'shearzones')
+    )
 }
