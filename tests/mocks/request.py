5cd47196f248d256cfb7f2b88989b7fc2205444a
Jess Robertson <jesse.robertson@csiro.au>
2015-01-24 08:27:16 +0800

    PYSISS-30: Adding mock requests

---
+++ tests/mocks/request.py
@@ -0,0 +1,7 @@
+""" file: update_mocks.py
+	author: Jess Robertson
+			CSIRO Mineral Resources Research Flagship
+	date:   January 2015
+
+	description: Update the mock files
+"""
---
+++ tests/mocks/update.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+""" file: update_mocks.py
+	author: Jess Robertson
+			CSIRO Mineral Resources Research Flagship
+	date:   January 2015
+
+	description: Update the mock files
+"""
+
+if __name__ == '__main__':
+	main()
