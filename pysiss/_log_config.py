802b79ef538a1b44019667f7fa864acef4cd3062
Jess Robertson <jesse.robertson@csiro.au>
2015-02-02 11:06:37 +0800

    Fixing log messages

--- pysiss/_log_config.py
+++ pysiss/_log_config.py
@@ -1,18 +1,16 @@
 LOG_CONFIG = {
     'version': 1,
     'loggers': {
-        'pysiss': {'handlers': ['console', 'file']}
+        'pysiss': {'handlers': ['console']}
     },
     'formatters': {
-        'brief': {'format': "pysiss - %(module)-8s - %(message)s"},
+        'brief': {'format': "(pysiss) %(module)-8s - %(levelname)-8s" \
+                            " %(message)s"},
         'detailed': {'format': '%(asctime)s %(module)-17s line:%(lineno)-4d ' \
                      '%(levelname)-8s %(message)s'}
     },
     'handlers': {
         'console': {'class': 'logging.StreamHandler',
-                    'formatter': 'brief', 'stream': 'ext://sys.stdout'},
-        'file': {'class': 'logging.handlers.RotatingFileHandler',
-                 'formatter': 'detailed', 'filename': 'logs/pysiss.log',
-                 'maxBytes': 2 ** 15, 'level': 'DEBUG'}
+                    'formatter': 'brief', 'stream': 'ext://sys.stdout'}
     }
 }
