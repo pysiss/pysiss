6cd2b56545425a1dfbaa0a6d9c52d2f2e2f02997
Jess Robertson <jesse.robertson@csiro.au>
2015-05-04 10:12:14 +0800

    Merge id_object and metadata_object

--- pysiss/objects.py
+++ pysiss/objects.py
@@ -1,4 +1,4 @@
-""" file: objects.py (pysiss)
+""" file: metadata_object.py (pysiss)
     author: Jess Robertson
             CSIRO Earth Science and Resource Engineering
     email:  jesse.robertson@csiro.au
@@ -11,33 +11,12 @@ from __future__ import print_function, division
 
 import uuid
 
-
-class id_object(object):
-
-    """ A mixin class to implement UUID comparisons for child classes
-
-        This metaclass generates a UUID for a class at initialization,
-        and defines the class __eq__ method to use this UUID.
-    """
-
-    def __init__(self, ident=None, *args, **kwargs):
-        super(id_object, self).__init__(*args, **kwargs)
-        if ident:
-            self.uuid = uuid.uuid5(uuid.NAMESPACE_DNS, ident)
-        else:
-            self.uuid = uuid.uuid4()
-
-    def __eq__(self, other):
-        """ Equality test
-
-            Class instances are equal if their UUIDs match
-        """
-        return self.uuid == other.uuid
-
-
 class metadata_object(id_object):
 
     """ A mixin class to store metadata about an object
+
+        This metaclass generates a UUID for a class at initialization,
+        and defines the class __eq__ method to use this UUID.
 
         This metaclass generates a metadata record for a class at
         initialization, and defines a set of methods for serializing and
@@ -46,7 +25,22 @@ class metadata_object(id_object):
 
     def __init__(self, ident=None, *args, **kwargs):
         super(metadata_object, self).__init__(*args, **kwargs)
-        if hasattr(self, __metadata_tag__):
-            self.metadata = Metadata(tag=self.__metadata_tag__)
+        if ident:
+            self.uuid = uuid.uuid5(uuid.NAMESPACE_DNS, ident)
         else:
-            self.metadata = Metadata()
+            self.uuid = uuid.uuid4()
+            self.ident = self.uuid
+
+        # Construct metadata attributes
+        if hasattr(self, __metadata_tag__):
+            self.metadata = Metadata(ident=self.ident,
+                                     tag=self.__metadata_tag__)
+        else:
+            self.metadata = Metadata(ident=self.ident)
+
+    def __eq__(self, other):
+        """ Equality test
+
+            Class instances are equal if their UUIDs match
+        """
+        return self.uuid == other.uuid
