61bb65e0ed747d20df32ce3991f1e34105af2846
Jess Robertson <jesse.robertson@csiro.au>
2015-05-18 14:02:53 +0800

    Improving NamespaceMap representation

--- pysiss/metadata/namespaces.py
+++ pysiss/metadata/namespaces.py
@@ -10,7 +10,7 @@
 
 from __future__ import print_function, division
 
-from lxml.etree import QName
+from lxml import etree
 
 
 class NamespaceMap(dict):
@@ -29,7 +29,6 @@ class NamespaceMap(dict):
 
     def __init__(self, *namespaces, **kwargs):
         super(NamespaceMap, self).__init__()
-        self._cached = False
         self.inverse = dict(reversed(item) for item in self.items())
 
         # Init with namespace dictionaries
@@ -37,7 +36,7 @@ class NamespaceMap(dict):
             try:
                 # If we've been tossed a tag, split it first
                 # Will raise a ValueError if not a Name
-                qname = QName(namespace)
+                qname = etree.QName(namespace)
                 if qname.namespace:
                     self.add_from_uri(qname.namespace)
                 else:
@@ -49,8 +48,18 @@ class NamespaceMap(dict):
         if kwargs:
             self.update(kwargs)
 
+        # Init internal variables
+        self._cached = False
+        self._stored_uris, self._stored_namespaces = set(), set()
+
+    def __str__(self):
+        return super(NamespaceMap, self).__str__()
+
+    def __repr__(self):
+        return super(NamespaceMap, self).__repr__()
+
     def __setitem__(self, key, value):
-        """ Associate the given shortened namespace with the given 
+        """ Associate the given shortened namespace with the given
             namespace URI.
 
             If key is None, 'None', 'none' or '', then a key is generated, i.e.
@@ -146,7 +155,7 @@ class NamespaceMap(dict):
                 'Unknown namespace key {0}'.format(namespace)
                 + ' in tag {0}'.format(tag))
         else:
-            return QName(self[namespace], localname)
+            return etree.QName(self[namespace], localname)
 
     def shorten(self, tag):
         """ Return a tag with a namespace in shortened form.
@@ -160,38 +169,37 @@ class NamespaceMap(dict):
                     or not (default is True for a short namespace).
 
             Returns:
-                an lxml QName string which contains a regularized tag name
+                an lxml etree.QName string which contains a regularized tag name
         """
         # Check input tag, try to assign a namespace and value
-        tag = QName(tag)
-        if tag.namespace not in (None, '', 'None'):
-            # Check whether we should add the uri
-            if tag.namespace not in self.stored_namespace_uris:
-                print('Adding new namespace url {0}'.format(tag.namespace),
-                      'keys: ', list(self.keys()))
-                self.add_from_uri(tag.namespace)
-
-            return QName(self.inverse[tag.namespace],
-                         tag.localname)
-        else:
+        try:
+            qname = etree.QName(tag)
+            if qname.namespace not in (None, '', 'None'):
+                # Check whether we should add the uri
+                if qname.namespace not in self.stored_namespace_uris:
+                    self.add_from_uri(qname.namespace)
+                return self.inverse[qname.namespace] + ':' + qname.localname
+            else:
+                raise ValueError()
+        except ValueError:
             return tag
 
     def add_from_tag(self, tag):
-        """ Add a namespace from an lxml tag. 
+        """ Add a namespace from an lxml tag.
 
-            The namespace is denoted using lxml's `{namespace}tag` qname 
+            The namespace is denoted using lxml's `{namespace}tag` qname
             specification. This is really just a convenience wrapper -
-            the tag is passed to lxml.QName and then the namespace is passed 
+            the tag is passed to lxml.etree.QName and then the namespace is passed
             off to the `add_from_uri` method. To see what the shortened namespace
             will look like then check out the documentation for `add_from_uri`.
 
             If the tag has no namespace then nothing is done
 
-            Parameters: 
+            Parameters:
                 tag - the (namespaced) tag containing the tag to be added
         """
         try:
-            qname = QName(tag)
+            qname = etree.QName(tag)
             if qname.namespace:
                 self.add_from_uri(qname.namespace)
         except ValueError:
